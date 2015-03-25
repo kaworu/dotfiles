0d880f2c2fcc00dbe969c3bf64c87780adcde431
Alexandre Perrin <alex@kaworu.ch>
2015-03-25 15:34:06 +0100

    bye bye gnome-sound-applet

--- .i3/autostart/100-gnome-session.sh
+++ .i3/autostart/100-gnome-session.sh
@@ -4,5 +4,4 @@
 
 gnome-settings-daemon &      # handles themes, starts gnome-screensaver. You may have to use gconf to disable it setting the background.
 nm-applet &                  # assuming you're using Network Manager
-gnome-sound-applet &         # manage the speaker volume.
 eval `gnome-keyring-daemon`  # SSH/GPG agent
