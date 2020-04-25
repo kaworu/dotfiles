1de70c795ab93a78f2c03eac339f66aa71a76c01
Alexandre Perrin <alex@kaworu.ch>
2020-04-25 23:31:03 +0200

    no more autostart xrandr
     Let's use autorandr.

--- .i3/autostart/001-xrandr.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-screens="$(xrandr -q | awk '/ connected / {print $1}')"
-if [ "$(echo "$screens" | wc -l)" -eq 2 ]; then
-    xrandr --output "$(echo "$screens" | awk 'NR == 2 {print}')" --pos 1920x0 \
-           --output "$(echo "$screens" | awk 'NR == 1 {print}')" --pos 0x360
-fi
